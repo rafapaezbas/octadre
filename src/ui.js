399db66d262d42bcac2807f9a7f70ed9ea23e8dd
root <email@email.com>
2021-02-21 16:33:10 +0100

    fixed identations

--- src/ui.js
+++ src/ui.js
@@ -2,31 +2,31 @@ const { app, BrowserWindow  } = require('electron');
 
 exports.setupUI = () => {
 
-    app.whenReady().then(createWindow);
+	app.whenReady().then(createWindow);
 
-    app.on('window-all-closed', () => {
-        if (process.platform !== 'darwin') {
-            app.quit();
-        }
-    });
+	app.on('window-all-closed', () => {
+		if (process.platform !== 'darwin') {
+			app.quit();
+		}
+	});
 
-    app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) {
-            createWindow();
-        }
-    });
+	app.on('activate', () => {
+		if (BrowserWindow.getAllWindows().length === 0) {
+			createWindow();
+		}
+	});
 };
 
 const createWindow = () => {
-    const win = new BrowserWindow({
-        width: 570,
-        height: 190,
-        webPreferences: {
-            nodeIntegration: true,
-            enableRemoteModule: true
-        },
-        frame: false
-    });
-    win.setMenuBarVisibility(false);
-    win.loadFile('./ui/index.html');
+	const win = new BrowserWindow({
+		width: 570,
+		height: 190,
+		webPreferences: {
+			nodeIntegration: true,
+			enableRemoteModule: true
+		},
+		frame: false
+	});
+	win.setMenuBarVisibility(false);
+	win.loadFile('./ui/index.html');
 };
